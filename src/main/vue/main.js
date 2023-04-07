cb22d408638c295c48fae08e1dff88125c542f67
KiTae Lee <vivaoul@naver.com>
2023-04-07 18:29:26 +0900

    feat. add "quasar" ui framework

--- src/main/vue/main.js
+++ src/main/vue/main.js
@@ -1,9 +1,17 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import { Quasar } from 'quasar'
+// Import icon libraries
+import '@quasar/extras/material-icons/material-icons.css'
+// Import Quasar css
+import 'quasar/src/css/index.sass'
 
 import './assets/main.css'
 import router from "@/route";
+import App from './App.vue'
 
 const app = createApp(App)
 app.use(router)
+app.use(Quasar, {
+    plugins: {}
+})
 app.mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,19 @@ import {fileURLToPath, URL} from 'node:url'
 
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     base: "/static/vue/",
     root: "src/main/vue",
     plugins: [
-        vue()
+        vue({
+            template: { transformAssetUrls }
+        }),
+        quasar({
+            sassVariables: 'src/main/vue/quasar-variables.sass'
+        })
     ],
     resolve: {
         alias: {
